1c21b6481e32ab5be15acf0aee34e3ccf0e7e6fb
kerolosheba <57691220+kerolosheba@users.noreply.github.com>
2022-10-16 04:08:54 +0200

    Password Generator

---
+++ index.js
@@ -0,0 +1,37 @@
+const charactersAll = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+ "/"]
+
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const charactersNumbers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const charactersSymboles = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+ "/"]
+
+const numbers = document.querySelector('#Numbers')
+const symboles = document.querySelector('#Symboles')
+
+let passwordEl = document.getElementById("password-el")
+
+let lengthEl = document.getElementById("length-el")
+
+function generatePassword() {
+ passwordEl.textContent = ""
+ if (numbers.checked === false && symboles.checked === false) {
+  for (let i = 0; i < lengthEl.valueAsNumber; i++) {
+   passwordEl.textContent += characters[Math.floor(Math.random()*characters.length)]
+  }
+ } else if (numbers.checked === true && symboles.checked === false) {
+  for (let i = 0; i < lengthEl.valueAsNumber; i++) {
+   passwordEl.textContent += charactersNumbers[Math.floor(Math.random() * charactersNumbers.length)]
+  }
+ } else if (numbers.checked === false && symboles.checked === true) {
+  for (let i = 0; i < lengthEl.valueAsNumber; i++) {
+   passwordEl.textContent += charactersSymboles[Math.floor(Math.random() * charactersSymboles.length)]
+  }
+ } else {
+  for (let i = 0; i < lengthEl.valueAsNumber; i++) {
+   passwordEl.textContent += charactersAll[Math.floor(Math.random() * charactersAll.length)]
+  }
+ }
+}
